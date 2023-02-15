a9500758a2fbcc327af5601a409445858c2eca32
sparkle6979 <liuhl79@163.com>
2023-02-15 10:33:25 +0800

    PropHolder UPDATE

--- src/main/java/util/PropHolder.java
+++ src/main/java/util/PropHolder.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropHolder {
-    private static Properties properties = null;
+    // 懒汗式单例
+    private volatile static Properties properties = null;
 
-    public static synchronized Properties getProperties() throws IOException {
-        if(properties == null){
-            properties = new Properties();
-            properties.load(PropHolder.class.getClassLoader().getResourceAsStream("datacollection.properties"));
+    public static Properties getProperties() throws IOException {
+        if (properties == null) {
+            synchronized (Properties.class) {
+                if (properties == null) {
+                    properties = new Properties();
+                    properties.load(PropHolder.class.getClassLoader().getResourceAsStream("datacollection.properties"));
+                }
+            }
         }
         return properties;
     }
-
 }
